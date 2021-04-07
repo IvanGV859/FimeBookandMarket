4f31bbdea51a1452d043bbe242ea751a17bb0367
Ivan Gonzalez <se_ivan_859@hotmail.com>
2021-04-07 14:26:27 -0500

    Home

---
+++ app/src/main/java/com/example/fimebookandmarket/Home.java
@@ -0,0 +1,35 @@
+package com.example.fimebookandmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Home extends AppCompatActivity {
+
+    Button btn1,btn2,btn3,btn4,btn6,btn7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this,"Editar Perfil", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/fimebookandmarket/SignIn.java
+++ app/src/main/java/com/example/fimebookandmarket/SignIn.java
@@ -2,13 +2,28 @@ package com.example.fimebookandmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
+
+    Button btnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btnHome = findViewById(R.id.Signin);
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignIn.this,Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
