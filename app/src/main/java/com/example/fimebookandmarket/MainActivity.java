d9f71f339912284f11b2ea116fbe3cc870806e13
Ivan Gonzalez <se_ivan_859@hotmail.com>
2021-04-07 10:55:12 -0500

    Pantalla principal, de registro e inicio de sesion

--- app/src/main/java/com/example/fimebookandmarket/MainActivity.java
+++ app/src/main/java/com/example/fimebookandmarket/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.fimebookandmarket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignin = findViewById(R.id.btnSignin);
+
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this,SignIn.class);
+                startActivity(signIn);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register = new Intent(MainActivity.this,Register.class);
+                startActivity(register);
+            }
+        });
+
     }
 }
