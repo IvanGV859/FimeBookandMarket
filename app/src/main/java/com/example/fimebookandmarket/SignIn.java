a7130f1a3db7b005082143450072df4089ca5bd5
Pedro Candia <52978459+AilingITS@users.noreply.github.com>
2021-04-14 11:07:18 -0500

    Login y registro funcional

--- app/src/main/java/com/example/fimebookandmarket/SignIn.java
+++ app/src/main/java/com/example/fimebookandmarket/SignIn.java
@@ -1,21 +1,33 @@
 package com.example.fimebookandmarket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
 
     private EditText mEditTextEmail, mEditTextPass;
 
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mEditTextEmail = findViewById(R.id.edtEmail);
         mEditTextPass = findViewById(R.id.edtPass);
@@ -25,9 +37,53 @@ public class SignIn extends AppCompatActivity {
     public void onClick(View view){
         switch (view.getId()){
             case R.id.Signin:
-                Intent intent = new Intent(SignIn.this,Home.class);
-                startActivity(intent);
+                userLogin();
+                /*Intent intent = new Intent(SignIn.this,Home.class);
+                startActivity(intent);*/
                 break;
         }
+    }
+
+    private void userLogin(){
+
+        String email = mEditTextEmail.getText().toString().trim();
+        String password = mEditTextPass.getText().toString().trim();
+
+        if(email.isEmpty()){
+            mEditTextEmail.setError("Se requiere un correo");
+            mEditTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            mEditTextEmail.setError("Ingrese un correo valido");
+            mEditTextEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            mEditTextPass.setError("Se requiere una contraseña");
+            mEditTextPass.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6){
+            mEditTextPass.setError("Ingrese minimo 6 caracteres en la contraseña");
+            mEditTextPass.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(SignIn.this,Home.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(SignIn.this, "Usuario no registrado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
