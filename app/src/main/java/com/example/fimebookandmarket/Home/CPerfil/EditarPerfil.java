bfddad9adeddb37f2f80e9ff4d216a7be73a32da
Pedro Candia <52978459+AilingITS@users.noreply.github.com>
2021-04-22 14:04:48 -0500

    EditarPerfil(No funcional)

--- app/src/main/java/com/example/fimebookandmarket/Home/CPerfil/EditarPerfil.java
+++ app/src/main/java/com/example/fimebookandmarket/Home/CPerfil/EditarPerfil.java
@@ -1,4 +1,4 @@
-package com.example.fimebookandmarket.Home;
+package com.example.fimebookandmarket.Home.CPerfil;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,11 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.fimebookandmarket.Home.CPerfil.EditarPerfil;
-import com.example.fimebookandmarket.Inicio.MainActivity;
-import com.example.fimebookandmarket.Inicio.Register;
-import com.example.fimebookandmarket.Inicio.SignIn;
+import com.example.fimebookandmarket.Home.Perfil;
 import com.example.fimebookandmarket.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -20,9 +18,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class Perfil extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
 
-    private TextView mTextViewNombreP, mTextViewEmailP, mTextViewPhoneP;
+public class EditarPerfil extends AppCompatActivity {
+
+    private TextView mEditTextNombreP, mEditTextPhoneP;
 
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
@@ -30,14 +31,14 @@ public class Perfil extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil);
+        setContentView(R.layout.activity_editar_perfil);
 
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        mTextViewNombreP = findViewById(R.id.usuarioPerfil);
-        mTextViewEmailP = findViewById(R.id.emailPerfil);
-        mTextViewPhoneP = findViewById(R.id.phonePerfil);
+        mEditTextNombreP = findViewById(R.id.editTextusuario);
+        mEditTextPhoneP = findViewById(R.id.editTextTextcelular);
+
 
 
         mDatabase.child("Users").addValueEventListener(new ValueEventListener() {
@@ -48,13 +49,10 @@ public class Perfil extends AppCompatActivity {
                     String id = mAuth.getCurrentUser().getUid();
 
                     String nombreP = dataSnapshot.child(id).child("usuario").getValue().toString();
-                    mTextViewNombreP.setText(nombreP);
-
-                    String emailP = dataSnapshot.child(id).child("email").getValue().toString();
-                    mTextViewEmailP.setText(emailP);
+                    mEditTextNombreP.setText(nombreP);
 
                     String phoneP = dataSnapshot.child(id).child("phone").getValue().toString();
-                    mTextViewPhoneP.setText(phoneP);
+                    mEditTextPhoneP.setText(phoneP);
 
                 }
             }
@@ -62,14 +60,22 @@ public class Perfil extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
+
+        String Name = mEditTextNombreP.getText().toString();
+        String phone = mEditTextPhoneP.getText().toString();
     }
 
     public void onClick(View view){
         switch (view.getId()){
-            case R.id.btnEditarPerfilP:
-                Intent Eperfil = new Intent(Perfil.this, EditarPerfil.class);
-                startActivity(Eperfil);
+            case R.id.btnGuardarE:
+
+
+
+                Intent perfilP = new Intent(EditarPerfil.this, Perfil.class);
+                startActivity(perfilP);
                 break;
         }
     }
+
+
 }
--- app/src/main/java/com/example/fimebookandmarket/Home/Menu.java
+++ app/src/main/java/com/example/fimebookandmarket/Home/Menu.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import Libros;
+//import Libros;
 import com.example.fimebookandmarket.R;
 
 public class Menu extends AppCompatActivity {
